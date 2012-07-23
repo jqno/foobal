00c3869bb46cd20d10f7eb9f11bd433dfd1a1db9
Jan Ouwens <jan.ouwens@gmail.com>
2012-07-23 11:21:02 +0200

    Handle updating of existing XML

--- src/main/scala/nl/jqno/foobal/updateoutcomes/OutcomesUpdater.scala
+++ src/main/scala/nl/jqno/foobal/updateoutcomes/OutcomesUpdater.scala
@@ -1,9 +1,10 @@
 package nl.jqno.foobal.updateoutcomes
 
+import scala.collection.mutable
+
+import nl.jqno.foobal.domain.Outcome
 import nl.jqno.foobal.io.Downloader
 import nl.jqno.foobal.io.Files
-import nl.jqno.foobal.io.HtmlParser
-import nl.jqno.foobal.io.DateFactory
 import nl.jqno.foobal.io.HtmlParser
 import nl.jqno.foobal.io.Url
 
@@ -13,9 +14,12 @@ class OutcomesUpdater(
     files: Files = new Files) {
   
   def update(url: Url, fileName: String): Unit = {
+    val existing = files.importFrom(fileName) getOrElse Nil
+    
     downloader.fetch(url) foreach { html =>
-      val outcomes = parser.parse(html)
-      files.exportTo(fileName, outcomes)
+      val incoming = parser.parse(html)
+      val outcomes = existing ++ incoming
+      files.exportTo(fileName, outcomes.distinct)
     }
   }
 }
--- src/test/scala/nl/jqno/foobal/updateoutcomes/OutcomesUpdaterTest.scala
+++ src/test/scala/nl/jqno/foobal/updateoutcomes/OutcomesUpdaterTest.scala
@@ -23,10 +23,11 @@ import nl.jqno.foobal.io.Files
 import nl.jqno.foobal.io.HtmlParser
 import com.nummulus.boite.Box
 import nl.jqno.foobal.io.Url
+import java.io.FileNotFoundException
+import org.joda.time.LocalDate
 
 @RunWith(classOf[JUnitRunner])
 class OutcomesUpdaterTest extends FlatSpec with ShouldMatchers with OneInstancePerTest with MockitoSugar {
-  val url = mock[Url]
   val files = mock[Files]
   val parser = mock[HtmlParser]
   val downloader = mock[Downloader]
@@ -35,33 +36,71 @@ class OutcomesUpdaterTest extends FlatSpec with ShouldMatchers with OneInstanceP
   behavior of "An OutcomesUpdater"
   
   it should "download outcomes and update the XML file" in {
-    val fileName = "/some/file_name"
+    createEmptyFile
     upload(Full(VALID_1_HTML), VALID_1_OUTCOMES)
     
-    updater.update(url, fileName)
+    update
     
-    verify(files).exportTo(fileName, VALID_1_OUTCOMES)
+    verifyWritten(VALID_1_OUTCOMES)
   }
   
   it should "download other outcomes and update the XML file" in {
-    val fileName = "/another/file_name"
+    createEmptyFile
     upload(Full(VALID_2_HTML), VALID_2_OUTCOMES)
     
-    updater.update(url, fileName)
+    update
     
-    verify(files).exportTo(fileName, VALID_2_OUTCOMES)
+    verifyWritten(VALID_2_OUTCOMES)
+  }
+  
+  it should "update the XML if a file is already present" in {
+    createFile(VALID_1_OUTCOMES)
+    upload(Full(VALID_2_HTML), VALID_2_OUTCOMES)
+    
+    update
+    
+    verifyWritten(VALID_1_OUTCOMES ++ VALID_2_OUTCOMES);
+  }
+  
+  it should "update an existing XML without duplicates" in {
+    val anotherOne = Outcome("Club A", "Club B", 2, 2, new LocalDate(2012, 7, 23))
+    
+    createFile(List(anotherOne, VALID_1_OUTCOMES(0)))
+    upload(Full(VALID_1_HTML), VALID_1_OUTCOMES)
+    
+    update
+    
+    verifyWritten(anotherOne :: VALID_1_OUTCOMES)
   }
   
   it should "not update the XML if the downloader fails" in {
+    createEmptyFile
     upload(Failure("uh-oh"))
     
-    updater.update(url, "")
+    update
     
-    verify(files, never).exportTo(anyString, any(classOf[List[Outcome]]))
+    verifyNothingWritten
   }
   
-  def upload(content: Box[String], result: List[Outcome] = Nil) = {
+  val url = mock[Url]
+  val fileName = "/some/filename"
+  
+  private def createEmptyFile =
+    when(files importFrom fileName) thenReturn Failure(new FileNotFoundException)
+  
+  private def createFile(result: List[Outcome]) =
+    when(files importFrom fileName) thenReturn Full(result)
+  
+  private def upload(content: Box[String], result: List[Outcome] = Nil) {
     when(downloader fetch url) thenReturn content
     content.foreach { s => when(parser parse s) thenReturn result }
   }
+  
+  private def update = updater.update(url, fileName)
+  
+  private def verifyWritten(xs: List[Outcome]) =
+    verify(files).exportTo(fileName, xs)
+  
+  private def verifyNothingWritten =
+    verify(files, never).exportTo(anyString, any(classOf[List[Outcome]]))
 }
