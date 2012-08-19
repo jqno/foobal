9f1b73614a014ad05c23a693a80fea09969c2c32
Jan Ouwens <jan.ouwens@nummulus.com>
2012-08-19 22:15:50 +0200

    Got rid of MainForManualTesting

--- src/main/scripts/foobal.sh
+++ src/main/scripts/foobal.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-java -jar foobal.jar "$@"
+if [ "$1" = "update" ]
+then
+  java -jar foobal.jar update http://www.fcupdate.nl/programma-uitslagen/s819/nederland-eredivisie-2012-2013/ $2 $3
+else
+  java -jar foobal.jar "$@"
+fi
