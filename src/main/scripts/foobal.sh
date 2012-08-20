0d01d1dca0cb4fd107475728dbe0c6ed99b89a16
Jan Ouwens <jan.ouwens@gmail.com>
2012-08-20 21:14:30 +0200

    Improved usability of shell script somewhat

--- src/main/scripts/foobal.sh
+++ src/main/scripts/foobal.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-if [ "$1" = "update" ]
+if [ $# -eq 0 ]
 then
-  java -jar foobal.jar update http://www.fcupdate.nl/programma-uitslagen/s819/nederland-eredivisie-2012-2013/ $2 $3
+  java -jar foobal.jar update http://www.fcupdate.nl/programma-uitslagen/s819/nederland-eredivisie-2012-2013/ outcomes.xml
+elif [ $# -eq 2 ]
+then
+  java -jar foobal.jar predict outcomes.xml "$1" "$2"
 else
   java -jar foobal.jar "$@"
 fi
