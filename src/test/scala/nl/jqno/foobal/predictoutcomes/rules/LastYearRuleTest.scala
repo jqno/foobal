d2174dab6de4d308e81cf81e47da7cf53fb12db1
Jan Ouwens <jan.ouwens@gmail.com>
2012-08-20 21:36:06 +0200

    Extracted trait for rule tests

--- src/test/scala/nl/jqno/foobal/predictoutcomes/rules/LastYearRuleTest.scala
+++ src/test/scala/nl/jqno/foobal/predictoutcomes/rules/LastYearRuleTest.scala
@@ -2,45 +2,37 @@ package nl.jqno.foobal.predictoutcomes.rules
 
 import org.joda.time.LocalDate
 import org.junit.runner.RunWith
-import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.matchers.ShouldMatchers
 
 import nl.jqno.foobal.domain.Outcome
-import nl.jqno.foobal.predictoutcomes.DroolsPredicter
 
 @RunWith(classOf[JUnitRunner])
-class LastYearRuleTest extends FlatSpec with ShouldMatchers {
-  private val FileName = "last_year.drl"
+class LastYearRuleTest extends RuleTester {
+  override val FileName = "last_year.drl"
   
   
   behavior of """Rule "Last year's outcome""""
   
   it should "predict a previous outcome" in {
-    val history = List(
-      Outcome("NAC", "Willem II", 1, 0, new LocalDate(2011, 9, 10)),
-      Outcome("PSV", "Willem II", 2, 0, new LocalDate(2011, 9, 11)),
-      Outcome("NAC", "PSV", 0, 1, new LocalDate(2011, 9, 12)))
+    history =
+      Outcome("NAC", "Willem II", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("PSV", "Willem II", 2, 0, new LocalDate(2011, 9, 11)) ::
+      Outcome("NAC", "PSV", 0, 1, new LocalDate(2011, 9, 12)) ::
+      Nil
     
-    assertLatestOutcome(history, 1, 0)
+    assertLatestOutcome(1, 0)
   }
   
   it should "select only the most recent outcome" in {
-    val history = List(
-      Outcome("NAC", "Willem II", 1, 0, new LocalDate(2011, 9, 10)),
-      Outcome("NAC", "Willem II", 2, 0, new LocalDate(2010, 9, 10)))
+    history =
+      Outcome("NAC", "Willem II", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("NAC", "Willem II", 2, 0, new LocalDate(2010, 9, 10)) ::
+      Nil
     
-    assertLatestOutcome(history, 1, 0)
+    assertLatestOutcome(1, 0)
   }
   
   it should "return 0-0 when there is no previous outcome" in {
-    assertLatestOutcome(List(), 0, 0)
-  }
-  
-  private def assertLatestOutcome(history: List[Outcome], homeScore: Int, outScore: Int) = {
-    val p = new DroolsPredicter("drl/" + FileName)
-    val result = p.predict(history, "NAC", "Willem II", new LocalDate(2012, 8, 20))
-    
-    result should be (Outcome("NAC", "Willem II", homeScore, outScore, new LocalDate(2012, 8, 20)))
+    assertLatestOutcome(0, 0)
   }
 }
---
+++ src/test/scala/nl/jqno/foobal/predictoutcomes/rules/RuleTester.scala
@@ -0,0 +1,28 @@
+package nl.jqno.foobal.predictoutcomes.rules
+
+import org.joda.time.LocalDate
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import nl.jqno.foobal.domain.Outcome
+import nl.jqno.foobal.predictoutcomes.DroolsPredicter
+import org.scalatest.OneInstancePerTest
+
+trait RuleTester extends FlatSpec with ShouldMatchers with OneInstancePerTest {
+  val FileName: String
+  
+  private val Date = new LocalDate(2012, 9, 10)
+  
+  private var _history: List[Outcome] = List()
+  protected[this] def history = _history
+  
+  def history_=(h: List[Outcome]) {
+    _history = h
+  }
+  
+  def assertLatestOutcome(homeScore: Int, outScore: Int) = {
+    val p = new DroolsPredicter("drl/" + FileName)
+    val result = p.predict(history, "NAC", "Willem II", Date)
+    
+    result should be (Outcome("NAC", "Willem II", homeScore, outScore, Date))
+  }
+}
