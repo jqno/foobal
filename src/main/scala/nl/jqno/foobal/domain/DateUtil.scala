7201023ad07358e55d80ea8a032b566c45f4b511
Jan Ouwens <jan.ouwens@nummulus.com>
2012-12-02 16:39:34 +0100

    Removed magic numbers

--- src/main/scala/nl/jqno/foobal/domain/DateUtil.scala
+++ src/main/scala/nl/jqno/foobal/domain/DateUtil.scala
@@ -4,17 +4,18 @@ import org.joda.time.LocalDate
 
 object DateUtil {
   val SeasonEndMonth = 7
+  private val LastDayOfSeasonEndMonth = 31
   
   def determineSeasonStartDateFor(date: LocalDate): LocalDate = {
     val thisYear = date.year.get
     val seasonStartYear = if (date.monthOfYear.get > SeasonEndMonth) thisYear else thisYear - 1
-    new LocalDate(seasonStartYear, 8, 1)
+    new LocalDate(seasonStartYear, SeasonEndMonth + 1, 1)
   }
   
   def determineSeasonEndDateFor(date: LocalDate): LocalDate = {
     val thisYear = date.year.get
     val seasonStartYear = if (date.monthOfYear.get > SeasonEndMonth) thisYear + 1 else thisYear
-    new LocalDate(seasonStartYear, 7, 31)
+    new LocalDate(seasonStartYear, SeasonEndMonth, LastDayOfSeasonEndMonth)
   }
   
   def determineSeasonEndYearFor(date: LocalDate): Int = {
