283391263bcdc2d711c51abb54fd08c9d69c3d94
Jan Ouwens <jan.ouwens@gmail.com>
2013-08-03 17:14:53 +0200

    Better rule for teams that didn't compete last year

--- src/test/scala/nl/jqno/foobal/predictoutcomes/rules/LastYearRuleTest.scala
+++ src/test/scala/nl/jqno/foobal/predictoutcomes/rules/LastYearRuleTest.scala
@@ -32,7 +32,41 @@ class LastYearRuleTest extends RuleTester {
     assertLatestOutcome(1, 0)
   }
   
-  it should "return 0-0 when there is no previous outcome" in {
+  it should "return 0-1 when the home team has no matches last year" in {
+    history =
+      Outcome("Willem II", "Ajax", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("PSV", "Willem II", 2, 0, new LocalDate(2010, 9, 10)) ::
+      Nil
+    
+    assertLatestOutcome(0, 1)
+  }
+  
+  it should "return 1-0 when the out team has no matches last year" in {
+    history =
+      Outcome("NAC", "Ajax", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("PSV", "NAC", 2, 0, new LocalDate(2010, 9, 10)) ::
+      Nil
+    
+    assertLatestOutcome(1, 0)
+  }
+  
+  it should "return 0-0 if both teams have played, but not against each other (which never happens but still)" in {
+    history =
+      Outcome("NAC", "Ajax", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("Willem II", "Ajax", 2, 0, new LocalDate(2010, 9, 10)) ::
+      Outcome("Ajax", "NAC", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("Ajax", "Willem II", 2, 0, new LocalDate(2010, 9, 10)) ::
+      Nil
+    
+    assertLatestOutcome(0, 0)
+  }
+  
+  it should "return 0-0 when neither team competed last year" in {
+    history =
+      Outcome("Feyenoord", "Ajax", 1, 0, new LocalDate(2011, 9, 10)) ::
+      Outcome("PSV", "Feyenoord", 2, 0, new LocalDate(2010, 9, 10)) ::
+      Nil
+    
     assertLatestOutcome(0, 0)
   }
 }
