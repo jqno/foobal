da7b306ded1cfd174cf06c985570ea80b4ed28dc
Jan Ouwens <jan.ouwens@gmail.com>
2013-08-03 17:56:52 +0200

    Fixed the shell script

--- src/main/scripts/foobal.sh
+++ src/main/scripts/foobal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 ]
 then
-  java -jar foobal.jar updatehttp://www.fcupdate.nl/programma-uitslagen/s949/nederland-eredivisie-2013-2014/ 
+  java -jar foobal.jar update http://www.fcupdate.nl/programma-uitslagen/s949/nederland-eredivisie-2013-2014/ outcomes.xml
 elif [ $# -eq 2 ]
 then
   java -jar foobal.jar predict outcomes.xml "$1" "$2"
