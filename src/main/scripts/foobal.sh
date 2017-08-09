a4b33d3af1b7e07ab0415813ed4f3ca202a586fe
Jan Ouwens <jan.ouwens@gmail.com>
2017-08-09 20:02:03 +0200

    Updates data source

--- src/main/scripts/foobal.sh
+++ src/main/scripts/foobal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ $# -eq 0 ]
 then
-  java -jar foobal.jar update "http://www.fcupdate.nl/uitslagen/s1216/nederland-jupiler-league-2016-2017/" outcomes.xml
+  java -jar foobal.jar update "http://www.fcupdate.nl/uitslagen/s1271/nederland-eredivisie-2017-2018/" outcomes.xml
 elif [ $# -eq 2 ]
 then
   java -jar foobal.jar predict outcomes.xml "$1" "$2"
